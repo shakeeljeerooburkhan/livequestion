9f470f80ba9ce420d1aa5bb6c9c67e58b6ba9c4f
shakeeljeerooburkhan <shakeel.jeerooburkhan995@gmail.com>
2021-02-19 12:05:58 +0100

    Partie Integration Shakeel

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Live Question</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anomymous">
+		<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet"  href="css/style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="js/script.js"></script>
+		<!--Lien de la police d'écriture-->
+
+
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<!--Partie des 7 boutons avec fond rose-->
+		<section id="applications">
+			<div class="conatiner">
+				<div class="heading">
+					<h3>Etiam laot,<span class="surlignement">alique sceleris.</span></h3>
+					<p>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem<br>accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.
+					</p>
+				</div>
+				<p class="marques">
+					<a href="a" class="btn btn-default"><img src="images/marque1.jpg">.Kyan Boards</a>
+					<a href="a" class="btn btn-default"><img src="images/marque2.jpg">Atica</a>
+					<a href="a" class="btn btn-default"><img src="images/marque3.jpg">Treva</a>
+					<a href="a" class="btn btn-default"><img src="images/marque4.jpg">Kanba</a>
+					<br><br>
+					<a href="a" class="btn btn-default"><img src="images/marque5.jpg">Tvit Forms</a>
+					<a href="a" class="btn btn-default"><img src="images/marque6.jpg">Aven</a>
+					<a href="a" class="btn btn-default"><img src="images/marque7.jpg">Utosia</a>
+				</p>
+			</div>
+		</section>
+		<!--Partie FAQ avec les questions -->
+		<section id="questions">
+			<div class="conatiner">
+				<div class="heading">
+					<h3>FAQ</h3>
+					<p>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem<br>accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.
+					</p>
+				</div>
+				<div class="list-deroul-6">
+					<div class="deroul1">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><p class="title-dropdown">Can I upgrade later on?</p><span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">a</a></li>
+								<li><a href="#">b</a></li>
+								<li><a href="#">c</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="deroul2">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><p class="title-dropdown">Can I port my data from another provider?</p><span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">a</a></li>
+								<li><a href="#">b</a></li>
+								<li><a href="#">c</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="deroul3">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><p class="title-dropdown">Are my food photos stored forever in the cloud?</p><span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">a</a></li>
+								<li><a href="#">b</a></li>
+								<li><a href="#">c</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="deroul4">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><p class="title-dropdown">Who foots the bill for that?</p><span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">a</a></li>
+								<li><a href="#">b</a></li>
+								<li><a href="#">c</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="deroul5">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><p class="title-dropdown">What's the real cost?</p><span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">a</a></li>
+								<li><a href="#">b</a></li>
+								<li><a href="#">c</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="deroul6">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><p class="title-dropdown">Can my company request a custom plan?</p><span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">a</a></li>
+								<li><a href="#">b</a></li>
+								<li><a href="#">c</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="clear"></div>
+				<br>
+				<div class="demande-question">
+					<p class="txt-demande-question">
+						Still have unanswered questions?
+						<a href="lien-question"><span class="pink">Get in touch</span></a>
+					</p>
+				</div>
+			</div>
+		</section>
+
+		<!--Courbure-->
+		
+			<div class="courb-blue">
+				<div class="courb-white"></div>
+			</div>
+			
+		<!--Bas de la page-->
+		<section id="footer">
+			<div class="block-footer">
+				<footer>
+					<p class="contenu-footer">
+						<div class="white-divider"></div>
+						© 2019 Page protected by reCAPTCHA and subject to Google 's <strong>Privacy policy</strong> and <strong>Terms of Service</strong>
+						<img class="" src="images/links.jpg">
+					</p>
+				</footer>
+			</div>
+			
+		</section>
+	</body>
+</html>
