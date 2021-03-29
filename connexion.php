4a5a70dd686f600de1c171e8decc487654046b3b
Valentin <cariavalentin95@gmail.com>
2021-03-29 08:56:39 +0200

    Create connexion.php

---
+++ connexion.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Live Question</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="conex.css">
+</head>
+<body>
+	
+	<section id="back" class="container-fluid">
+		<div id="pose" class="row">
+				<form id="pose2" method="POST" action="#">
+					<h2 id="pose3">connexion</h2>
+					<div class="colorMind">
+						<div id="pad">
+							<label class="pink" for="identifiant">identifiant:</label>
+							<a href="">recuperer un identifants oublier</a>
+							<input type="email" type="text" id="email" name="identifiant" class="form-control" placeholder="Votre email ou Votre pseudo">
+        				</div>
+        				<div id="pad2">
+							<label class="pink" for="password">mots de passe:</label>
+							<a href="">recuperer votre mots de passe oublier </a>
+							<input type="text" id="email" name="password" class="form-control" placeholder="votre mots de passes">
+        				</div>
+        				<div id="pad1">
+					 		<input id="connected" type="submit" name="submit" class="button1" value="Connexion">
+            				<a href="">créer un compte</a>
+        				</div>
+        			</div>
+				</form>
+        </div>
+	</section>
+</body>
+</html>
